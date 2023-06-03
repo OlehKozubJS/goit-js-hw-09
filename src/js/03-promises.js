ec037044eeccd2652d21b2727856d210d2fa707a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-03 23:39:35 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,16 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      setTimeout(() => {
+        resolve({position, delay});
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject({position, delay});
+      }, delay);
+    }
+  });
 }
 
 createPromise(2, 1500)
