c6fd3da71cbcb9cc39182c5b8f3f32656b67b69a
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2023-06-04 20:37:39 +0000

    Deploying to gh-pages from @ OlehKozubJS/goit-js-hw-09@1f2e85ce642dd86d18184732c0feed607d8b9f77 🚀

--- 03-promises.da76a3a8.js
+++
@@ -1,2 +0,0 @@
-var o,e;(o=2,e=1500,new Promise(((i,s)=>{Math.random()>.3?setTimeout((()=>{i({position:o,delay:e})}),e):setTimeout((()=>{s({position:o,delay:e})}),e)}))).then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.da76a3a8.js.map
