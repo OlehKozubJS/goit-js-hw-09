09605aabecb67767555da20a27b21a9272ca8e9f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-05 22:00:15 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,39 +5,28 @@ const delayStepInput = document.getElementsByName("step")[0];
 const amountInput = document.getElementsByName("amount")[0];
 const form = document.querySelector("form");
 
-let nextDelay;
-let delayStepData;
-let amountData;
-let createPromisesSI;
-
 form.addEventListener("submit", dataEnteringFunction);
 
 function dataEnteringFunction(e) {
   e.preventDefault();
 
-  nextDelay = Number(firstDelayInput.value);
-  delayStepData = Number(delayStepInput.value);
-  amountData = Number(amountInput.value);
+  let nextDelay = Number(firstDelayInput.value);
+  let delayStepData = Number(delayStepInput.value);
+  let amountData = Number(amountInput.value);
 
-  setTimeout(createPromises, nextDelay);
-
-  form.reset();
-}
-
-function createPromises() {
   for (let nextNumber = 1; nextNumber <= amountData; nextNumber += 1) {
-    setTimeout(createPromise, nextDelay);
-
     createPromise(nextNumber, nextDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-
+    .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    
     nextDelay += delayStepData;
   }
+
+  form.reset();
 }
 
 function createPromise(position, delay) {
@@ -54,3 +43,5 @@ function createPromise(position, delay) {
     }
   });
 }
+
+
